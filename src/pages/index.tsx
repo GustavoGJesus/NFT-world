9cb9e9ad969a9d16178e9e157d4b76cbd6b13310
GustavoGJesus <icarogabriel01@protonmail.com>
2021-12-26 14:44:12 -0300

    API Stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
 import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss'
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps{
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }) {
   return (
     <>
       <Head>
@@ -16,12 +25,30 @@ export default function Home() {
           <h1><span>NFT</span> world</h1>
           <p>
             Get access to information about the best NFT projects in 2022 
-            <span> for $10.00 month</span>.
+            <span> for {product.amount} month</span>.
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/planet.png" alt="" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KB027JHSTb4hNh8S4jwpuf5')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+  return{
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
 }
