95c2615e45c1002fbd1e3c284b4df0abe61f7526
GustavoGJesus <icarogabriel01@protonmail.com>
2021-12-30 17:45:31 -0300

    Events CLI Stripe: webhooks

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,11 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { stripe } from "../../services/stripe";
 
-
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST'){
-        
-
+    
         const checkoutSession = await stripe.checkout.sessions.create({
             
             payment_method_types:['card'],
